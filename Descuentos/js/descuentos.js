2d85de90608b7f92a4441edf7da2faa8f98e199d
victor <victorl_222@hotmail.com>
2021-08-26 13:18:58 -0300

    add cupons function

--- Descuentos/js/descuentos.js
+++ Descuentos/js/descuentos.js
@@ -7,19 +7,54 @@ const porcentajePrecioConDescuento = 100 ;
 function calcularPrecioConDescuento(precio,descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio *porcentajePrecioConDescuento)/100;
+    const select = document.querySelector("#cupones");
+   
+    switch (select.value) {
+        case "Dia del padre":
+            var descuentocupon= 10;
+            precioDescuentomascupon=precioConDescuento-descuentocupon
+            return precioDescuentomascupon
 
-    return precioConDescuento;
+            case "Dia del abuelo":
+            var descuentocupon= 20;
+            precioDescuentomascupon=precioConDescuento-descuentocupon
+            return precioDescuentomascupon
+
+            case "Dia del niño":
+            var descuentocupon= 50;
+            precioDescuentomascupon=precioConDescuento-descuentocupon
+            return precioDescuentomascupon
+        default:
+            break;
+    }
+
+    return precioConDescuento ;
 }
 
 function ButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
-    const priceValue = inputPrice.nodeValue;
+    const priceValue = inputPrice.value;
     const inputDiscount=document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.nodeValue;
+    const discountValue = inputDiscount.value;
     const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
-
-     const resultP = document.getElementById("ResultPrice");
+    const resultP = document.getElementById("ResultPrice");
+    
 
      resultP.innerText = "El precio con descuento son: $" + precioConDescuento
     
 }
+
+
+
+function cargarCupones(){
+    const cupones = ["Dia del padre", "Dia del niño", "Dia del abuelo"];
+
+        for (let index = 0; index < cupones.length; index++) {
+            var element = cupones[index];
+            document.getElementById("cupones").innerHTML += "<option value='"+cupones[index]+"'>"+cupones[index]+"</option>"; 
+
+        }
+        
+            
+}
+
